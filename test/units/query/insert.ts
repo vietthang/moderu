04366c395b053d5a0969f351bcd409fbea6c4e39
Yoshi <thangnv77@gmail.com>
2017-01-23 14:13:48 +0700

    Update insert test cases

--- test/units/query/insert.ts
+++ test/units/query/insert.ts
@@ -24,6 +24,13 @@ const knex = Knex({
   useNullAsDefault: true,
 });
 
+const now = new Expression(`strftime('%s', 'now')`, [], integer(), 'now');
+
+it('Should throw without model', () => {
+  const query = new InsertQuery(petTable.$meta);
+  assert.throw(() => query.toSQL(knex));
+});
+
 it('Should generate correct insert statement with simple model', () => {
   const query = new InsertQuery(petTable.$meta).value({ id: 1, name: 'abc' });
   const { sql, bindings } = query.toSQL(knex);
@@ -32,7 +39,6 @@ it('Should generate correct insert statement with simple model', () => {
 });
 
 it('Should generate correct insert statement with model contains expression', () => {
-  const now = new Expression(`strftime('%s', 'now')`, [], integer(), 'now');
   const query = new InsertQuery(petTable.$meta).value({ id: now, name: 'abc' });
   const { sql, bindings } = query.toSQL(knex);
   assert.deepEqual(bindings, ['abc']);
@@ -46,10 +52,41 @@ it('Should generate correct insert statement with set', () => {
   assert.equal('insert into "Pet" ("name") values (?)', sql);
 });
 
+it('Should generate correct insert statement with set by column', () => {
+  const query = new InsertQuery(petTable.$meta).set(petTable.name, 'abc');
+  const { sql, bindings } = query.toSQL(knex);
+  assert.deepEqual(bindings, ['abc']);
+  assert.equal('insert into "Pet" ("name") values (?)', sql);
+});
+
 it('Should generate correct insert statement with set to expression', () => {
-  const now = new Expression(`strftime('%s', 'now')`, [], integer(), 'now');
   const query = new InsertQuery(petTable.$meta).set('id', now);
   const { sql, bindings } = query.toSQL(knex);
   assert.deepEqual(bindings, []);
   assert.equal(`insert into "Pet" ("id") values (strftime('%s', 'now'))`, sql);
+});
+
+it('Should fail if set with invalid data', () => {
+  const query = new InsertQuery(petTable.$meta);
+  assert.throw(() => query.set('id', -1));
+});
+
+it('Should fail if set with expression but validation is disallow expression', () => {
+  const query = new InsertQuery(petTable.$meta).validationMode(1);
+  assert.throw(() => query.set('id', now));
+});
+
+it('Should success if set with valid data, without expression and validation is disallow expression', () => {
+  const query = new InsertQuery(petTable.$meta).validationMode(1);
+  assert.doesNotThrow(() => query.set('id', 1));
+});
+
+it('Should skip if set with invalid data but validation mode is disabled', () => {
+  const query = new InsertQuery(petTable.$meta).validationMode(2);
+  assert.doesNotThrow(() => query.set('id', -1));
+});
+
+it('Should fail if set with invalid validation mode', () => {
+  const query = new InsertQuery(petTable.$meta).validationMode(4);
+  assert.throw(() => query.set('id', 1));
 });
